afcdf2187c719479b8d1829301790daa9a00e168
Cyril <cyrillaugier@gmail.com>
2017-11-03 12:22:43 +0100

    ajout des résultats dans le logcat en commentaire

--- app/src/main/java/fr/minint/ppol/dostl/logcat/MainActivity.java
+++ app/src/main/java/fr/minint/ppol/dostl/logcat/MainActivity.java
@@ -30,11 +30,25 @@ public class MainActivity extends AppCompatActivity {
         Log.w(TAG, "Warning OnCreate Method");
         Log.e(TAG, "Error OnCreate Method");
 
+/* Résultat dans le logcat
+11-03 12:16:14.283 505-505/fr.minint.ppol.dostl.logcat V/MainActivity: Verbose OnCreate Method
+11-03 12:16:14.283 505-505/fr.minint.ppol.dostl.logcat D/MainActivity: Debug OnCreate Method
+11-03 12:16:14.284 505-505/fr.minint.ppol.dostl.logcat I/MainActivity: Info OnCreate Method
+11-03 12:16:14.284 505-505/fr.minint.ppol.dostl.logcat W/MainActivity: Warning OnCreate Method
+11-03 12:16:14.284 505-505/fr.minint.ppol.dostl.logcat E/MainActivity: Error OnCreate Method //ligne en rouge
+*/
         Log.d(VALUE, "msg : " + bjr + msg);
         Log.i(VALUE, a + " x " + b + " = " + mult);
         Log.i(VALUE, a + " / " + b + " = " + div +"," + mod + " (division Euclidienne)");
         Log.i(VALUE, a + " / " + b + " = " + f + " (en float)");
         Log.i(VALUE, a + " / " + b + " = " + d + " (en double)");
 
+/* Résultat dans le logcat :
+11-03 12:16:14.284 505-505/fr.minint.ppol.dostl.logcat D/Log: msg : Bonjour ! Je suis un message dans le Log.
+11-03 12:16:14.284 505-505/fr.minint.ppol.dostl.logcat I/Log: 5 x 3 = 15
+11-03 12:16:14.284 505-505/fr.minint.ppol.dostl.logcat I/Log: 5 / 3 = 1,2 (division Euclidienne)
+11-03 12:16:14.284 505-505/fr.minint.ppol.dostl.logcat I/Log: 5 / 3 = 1.6666666 (en float)
+11-03 12:16:14.284 505-505/fr.minint.ppol.dostl.logcat I/Log: 5 / 3 = 1.6666666666666667 (en double)
+*/
     }
 }
